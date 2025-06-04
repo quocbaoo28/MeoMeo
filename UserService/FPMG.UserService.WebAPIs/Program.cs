d9c034eae7357b469c9438a2496a1d6fdfd52398
tuankiet12012003 <kietdhtse172938@fpt.edu.vn>
2025-06-04 12:30:23 +0700

    feat: rabbitMQ for UserService

--- UserService/FPMG.UserService.WebAPIs/Program.cs
+++ UserService/FPMG.UserService.WebAPIs/Program.cs
@@ -1,9 +1,16 @@
+using FPMG.UserService.Services.Background;
+using FPMG.UserService.Services.Messaging;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSingleton<IEventPushlisher, EventPublisher>();
+builder.Services.AddSingleton<IEventConsumer, EventConsumer>();
+builder.Services.AddHostedService<EventConsumerBackgroundService>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
