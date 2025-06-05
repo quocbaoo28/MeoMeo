b0055732f5504eb0f8643db59ec3269ab1f44677
phuthang4444 <thangtpse151058@fpt.edu.vn>
2025-06-05 17:11:10 +0700

    fix: change User and Role table name into plural

--- UserService/FPMG.UserService.BussinessObjects/Roles.cs
+++ UserService/FPMG.UserService.BussinessObjects/Roles.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FPMG.UserService.BussinessObjects
 {
-    public class Role
+    public class Roles
     {
+        [Key]
         public int RoleId { get; set; }
         public string RoleName { get; set; }
-        public virtual ICollection<User> Users { get; set; }
+        public virtual ICollection<Users> Users { get; set; }
     }
 }
--- UserService/FPMG.UserService.DataAccessLayers/UserManagementDBContext.cs
+++ UserService/FPMG.UserService.DataAccessLayers/UserManagementDBContext.cs
@@ -14,42 +14,42 @@ namespace FPMG.UserService.DataAccessLayers
             : base(options)
         {
         }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Roles> Roles { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<User>()
+            modelBuilder.Entity<Users>()
                 .HasOne(u => u.Role)
                 .WithMany(r => r.Users)
                 .HasForeignKey(u => u.RoleId);
 
             // Seed Roles
-            modelBuilder.Entity<Role>().HasData(
-                new Role { RoleId = 1, RoleName = "Head" },
-                new Role { RoleId = 2, RoleName = "Examiner" },
-                new Role { RoleId = 3, RoleName = "Lecturer" },
-                new Role { RoleId = 4, RoleName = "Student" }
+            modelBuilder.Entity<Roles>().HasData(
+                new Roles { RoleId = 1, RoleName = "Head" },
+                new Roles { RoleId = 2, RoleName = "Examiner" },
+                new Roles { RoleId = 3, RoleName = "Lecturer" },
+                new Roles { RoleId = 4, RoleName = "Student" }
             );
 
             // Seed Users
-            modelBuilder.Entity<User>().HasData(
+            modelBuilder.Entity<Users>().HasData(
                 // Head
-                new User { UserId = 1, RoleId = 1, UserName = "head", Email = "head@fpmg.edu", Password = "123" },
+                new Users { UserId = 1, RoleId = 1, UserName = "head", Email = "head@fpmg.edu", Password = "123" },
 
                 // Examiner
-                new User { UserId = 2, RoleId = 2, UserName = "examiner", Email = "examiner@fpmg.edu", Password = "123" },
+                new Users { UserId = 2, RoleId = 2, UserName = "examiner", Email = "examiner@fpmg.edu", Password = "123" },
 
                 // Lecturers
-                new User { UserId = 3, RoleId = 3, UserName = "lecturer1", Email = "lecturer1@fpmg.edu", Password = "123" },
-                new User { UserId = 4, RoleId = 3, UserName = "lecturer2", Email = "lecturer2@fpmg.edu", Password = "123" },
+                new Users { UserId = 3, RoleId = 3, UserName = "lecturer1", Email = "lecturer1@fpmg.edu", Password = "123" },
+                new Users { UserId = 4, RoleId = 3, UserName = "lecturer2", Email = "lecturer2@fpmg.edu", Password = "123" },
 
                 // Students
-                new User { UserId = 5, RoleId = 4, UserName = "student1", Email = "student1@fpmg.edu", Password = "123" },
-                new User { UserId = 6, RoleId = 4, UserName = "student2", Email = "student2@fpmg.edu", Password = "123" },
-                new User { UserId = 7, RoleId = 4, UserName = "student3", Email = "student3@fpmg.edu", Password = "123" },
-                new User { UserId = 8, RoleId = 4, UserName = "student4", Email = "student4@fpmg.edu", Password = "123" },
-                new User { UserId = 9, RoleId = 4, UserName = "student5", Email = "student5@fpmg.edu", Password = "123" },
-                new User { UserId = 10, RoleId = 4, UserName = "student6", Email = "student6@fpmg.edu", Password = "123" }
+                new Users { UserId = 5, RoleId = 4, UserName = "student1", Email = "student1@fpmg.edu", Password = "123" },
+                new Users { UserId = 6, RoleId = 4, UserName = "student2", Email = "student2@fpmg.edu", Password = "123" },
+                new Users { UserId = 7, RoleId = 4, UserName = "student3", Email = "student3@fpmg.edu", Password = "123" },
+                new Users { UserId = 8, RoleId = 4, UserName = "student4", Email = "student4@fpmg.edu", Password = "123" },
+                new Users { UserId = 9, RoleId = 4, UserName = "student5", Email = "student5@fpmg.edu", Password = "123" },
+                new Users { UserId = 10, RoleId = 4, UserName = "student6", Email = "student6@fpmg.edu", Password = "123" }
             );
         }
 
