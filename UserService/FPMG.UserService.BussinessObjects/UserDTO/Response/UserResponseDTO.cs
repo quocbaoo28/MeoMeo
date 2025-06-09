708241427f2a955922c7c66a9d9b1fe6c67c7745
phuthang4444 <thangtpse151058@fpt.edu.vn>
2025-06-09 12:33:28 +0700

    fix: add UserRequest/ResponseDTO for UserService

---
+++ UserService/FPMG.UserService.BussinessObjects/UserDTO/Response/UserResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPMG.UserService.BussinessObjects.UserDTO.Response
+{
+    public class UserResponseDTO
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string RoleName { get; set; }
+    }
+}
