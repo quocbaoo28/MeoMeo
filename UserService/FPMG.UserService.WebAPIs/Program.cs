51c31fa2e8ada5d8a16c38ad02e9af64afcabe91
phuthang4444 <thangtpse151058@fpt.edu.vn>
2025-06-09 08:42:18 +0700

    feat: add GetAllUsers feature

--- UserService/FPMG.UserService.WebAPIs/Program.cs
+++ UserService/FPMG.UserService.WebAPIs/Program.cs
@@ -1,11 +1,22 @@
+using System.Text.Json.Serialization;
 using FPMG.UserService.DataAccessLayers;
+using FPMG.UserService.Repositories.Implementations;
+using FPMG.UserService.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()/*.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    // options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+})*/;
+#region CORS
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+#endregion
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<UserManagementDBContext>(options =>
